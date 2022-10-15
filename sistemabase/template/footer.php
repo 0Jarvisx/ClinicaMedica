99e4a049db5597245864ec77d7c507fdff6a2d24
Estefanyox <Vel211345@uvg.edu.gt>
2022-10-15 12:29:54 -0600

    Update footer.php

--- sistemabase/template/footer.php
+++ sistemabase/template/footer.php
@@ -1,13 +1,60 @@
-<footer class="py-3 my-4 footerCustom">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 1</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 2</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 3</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 4</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 5</a></li>
-    </ul>
-    <p class="text-center text-muted">&copy; Derechos de Autor y temas de leyes</p>
+<footer class="text-center text-white" style="background-color: #0a4275">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+          <!-- user hombre -->
+      <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/0Jarvisx" role="button" data-mdb-ripple-color="dark"><i class="fas fa-user-tie"></i>Javier Linares</a>
+     <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/yari2003"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fas fa-user"></i
+      >Yarissa Debroy</a>
+
+    <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/KingCJ23"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fas fa-user-tie"></i
+      >Carlos Juarez</a>
+
+          <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/Estefanyox"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fas fa-user"></i
+      >Estefany Yox</a>
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/0Jarvisx/ClinicaMedica/"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-github"></i
+      >Repositorio grupal</a>
+      <br>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright: 
+  </div>
+  <!-- Copyright -->
 </footer>
 </body>
+</html>
+</body>
 
+</html>
+</body>
 </html>
