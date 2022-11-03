bd69de0b65e08049762b317f8c827f7776a6d0ed
Yarissa Debroy <104816562+yari2003@users.noreply.github.com>
2022-11-02 20:07:37 -0600

    ACTUALIZACION DE PROYECTO COMPLETO

--- sistemabase/editar_perfil.php
+++ sistemabase/editar_perfil.php
@@ -5,46 +5,56 @@ if(!$_SESSION['user_id']){
     header("location: index.php");
 } 
 
+include_once("../../model/functions.php");
+
+
 ?>
 
 
 
-<head>
     <script src="assets/js/moduloUsuarios.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
-    <link rel="stylesheet" href="assets/css/style2.css">
-    <title>Perfil</title>
-</head>
+    <link rel="stylesheet" href="assets/css/editar.css">
+	<link rel="stylesheet" type="text/css"  href="assets/css/template.css" />
+    <link rel="stylesheet" type="text/css"  href="assets/css/status_server_modal.css" />
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-<body>
 
-<div>
-	<div>
-        <div class="col-md-4 well" id="leftPanel">
-            <div class="row">
-                <div class="col-md-12">
-					
-                	<div>
-        				<img heigth="70px" src="data:image/jpeg;base64,<?php echo base64_encode($_SESSION['foto']);?>" class="img-circle img-thumbnail"/>
-						
-                        <div> <?php echo $_SESSION['user_nombre']." ".$_SESSION['user_apellido'];
+
+
+<div class="limiter">
+
+<div class="container-login100">
+	<form class="foto" method="POST" action="foto.php">
+		<div class="wrap-login100">
+		<div class="login100-pic js-tilt" data-tilt>
+		<IMG src="assets/img/fotos/<?php echo $_SESSION['imagen'];?>" style="height:50PX" />
+				<div class="user"> <?php echo $_SESSION['user_nombre']." ".$_SESSION['user_apellido'];
                          ?></b></h4>
 						 <hr class="colorgraph">
+						 
+						 <input type="file"  id="imagen" name="imagen" class="form-control" >
+						<button class="img">Subir imagen</button>
+						
                     </div>
-        		  </div>
-        		</div>
-            </div>
-        </div>
-        <div class="col-md-8 well" id="rightPanel">
-            <div class="row">
-    <div class="col-md-12">
-    	<form align="center" class="form" method="POST" action="actualizar.php" enctype="multipart/form-data">
+					</form>
+</div>
+
+<form aling= center class="login100-form validate-form" method="POST" action="actualizar.php">
+				
+			<div class="editarPerfil">
+
 			<h2>Editar perfil </h2>
+			</div>
 			<hr class="colorgraph">
             
+			
 			<div class="row">
 				<div class="col-xs-12 col-sm-6 col-md-6">
 					<div class="form-group">
@@ -61,22 +71,23 @@ if(!$_SESSION['user_id']){
 			<div class="form-group">
 				<input type="nickname" name="user_upd" id="usuario" class="form-control" placeholder="<?php echo $_SESSION['username'];?>"  tabindex="4">
 			</div>
-			<div class="row">
+		
 				
 					<div class="form-group">
 						<input type="password" name="clave_upd" id="password" class="form-control" placeholder="Password" tabindex="5">
 					</div>
 		
-			</div>
+		
 			<hr class="colorgraph">
-				<div class="form"><button class="update">Actualizar datos</button></div>
-				<div class="form-group">
-				<input type="file" name="foto_upd" class="form-control-file"/>
-				</div>
-		</form>
-	</div>
-</div> 
-</body>
+				<div class="container-login100-form-btn"><button class="login100-form-btn">Actualizar datos</button></div>
+</div></div>
+			</form>
+</div>
+</div>
+</div>
+
 </html>
+
+
 
 
