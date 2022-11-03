1d4a497be683174176844e48441343d5116f86bc
Yarissa Debroy <104816562+yari2003@users.noreply.github.com>
2022-11-02 20:04:55 -0600

    ACTUALIZACION DE PROYECTO COMPLETO

--- sistemabase/template/userinfo.php
+++ sistemabase/template/userinfo.php
@@ -2,6 +2,7 @@
 include_once("../../controller/");
 ?>
 
+<link rel="stylesheet" href="assets/css/home.css">
 <header class="p-3 mb-3 border-bottom fondoHeader">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
@@ -9,21 +10,22 @@ include_once("../../controller/");
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <p> <?php echo $_SESSION['username'] ?> </p>
+            <p class="username"> <?php echo $_SESSION['user_nombre'] ?> </p>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input type="search" class="form-control" placeholder="Buscar..." aria-label="Search">
+        <form class="buscar">
+            <input type="search" class="buscar2" placeholder="Buscar..." aria-label="Search">
         </form>
 
         <div class="dropdown text-end">
             <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1"
                 data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="assets/img/logos/fotos/user.png" width="50" height="50" class="rounded-circle">
+                <img src="assets/img/logos/fotos/user.png <?php echo$_SESSION['user_imagen'] ?>" width="50" height="50" class="rounded-circle">
             </a>
             <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item"  href="configuracionF.php">Mi Perfil</a></li>
-                <li><a class="dropdown-item"  href="editar_perfil.php">Configuración</a></li>    
+
+                <li><a class="dropdown-item"  onclick="cargarContenido('editar_perfil.php')">Mi Perfil</a></li>
+                <li><a class="dropdown-item"  onclick="cargarContenido('editar_perfil.php')">Configuración</a></li>    
                 <li>
                     <hr class="dropdown-divider">
                 </li>
